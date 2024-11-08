a3d6ce7af083c3e2290b56eb3145cf6e3bcc5269
MCTGiang <143694286+MCTGiang@users.noreply.github.com>
2024-11-08 20:00:00 +0700

    Create Chapter 12 - Assignment Scraping.py

---
+++ 3. Course 03 - Using Python to Access Web Data/Module 04 - Chapter 12 Programs that Surf the Web/Chapter 12 - Assignment Scraping.py
@@ -0,0 +1,38 @@
+# Scraping Numbers from HTML using BeautifulSoup In this assignment you will write a Python program similar to http://www.py4e.com/code3/urllink2.py.
+# The program will use urllib to read the HTML from the data files below, and parse the data, extracting numbers and compute the sum of the numbers in the file.
+# Actual data: http://py4e-data.dr-chuck.net/comments_2122554.html (Sum ends with 40)
+# Data Format: The file is a table of names and comment counts. You can ignore most of the data in the file except for lines like the following:
+# <tr><td>Modu</td><td><span class="comments">90</span></td></tr>
+# <tr><td>Kenzie</td><td><span class="comments">88</span></td></tr>
+# <tr><td>Hubert</td><td><span class="comments">87</span></td></tr>
+# You are to find all the <span> tags in the file and pull out the numbers from the tag and sum the numbers.
+
+# Importing Libraries
+import urllib.request
+import urllib.parse
+import urllib.error
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+import re
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+# Input URL and Parse HTML
+url = input('Enter - ')
+html = urllib.request.urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, "html.parser")
+
+# Retrieve all of the anchor tags
+sum = 0
+tags = soup('span')
+for tag in tags:
+    y = str(tag)
+    x = re.findall("[0-9]+",y)
+    for i in x:
+        i = int(i)
+        sum = sum+i
+print(sum)
